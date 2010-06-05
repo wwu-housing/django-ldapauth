f6f32fa54575c8af88d655ef82c24bd41a49fcf8
huddlej <huddlej@987ccdad-c2ba-db11-a1c9-001321b30054>
2010-06-05 01:34:31 +0000

    added model to store sid/name pairs for ldapauth.
    git-svn-id: file:///usr/local/svn/django/trunk@4391 987ccdad-c2ba-db11-a1c9-001321b30054

--- models.py
+++ models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class LdapGroup(models.Model):
+    sid = models.CharField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
