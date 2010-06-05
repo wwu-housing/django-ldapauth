5a29fd31bebf0ebbb9e0590b190533ce5c9a4fbd
huddlej <huddlej@987ccdad-c2ba-db11-a1c9-001321b30054>
2010-06-05 01:39:48 +0000

    added comments to ldapauth
    git-svn-id: file:///usr/local/svn/django/trunk@4399 987ccdad-c2ba-db11-a1c9-001321b30054

--- backends.py
+++ backends.py
@@ -10,10 +10,18 @@ class LDAPBackend(ModelBackend):
     membership.
     """
     def get_group_permissions(self, user):
+        # Get locally stored group permissions.
         permissions_set = super(LDAPBackend, self).get_group_permissions(user)
+
         ldap = LDAP("wwu")
         ldap_person = ldap.get_person_by_username(user.username)
         groups = ldap_person.groups
+
+        # The traditional ModelBackend fetches all Permission instances
+        # associated with groups for which the current user is a member. This
+        # backend doesn't rely on local user/group relationships so it queries
+        # Permissions directly by group name instead of by group__user foreign
+        # key relationship.
         if len(groups) > 0:
             permissions = Permission.objects \
                 .filter(group__name__in=groups) \
@@ -21,4 +29,5 @@ class LDAPBackend(ModelBackend):
                 .order_by()
             permissions_set.update(set(["%s.%s" % (ct, name)
                                         for ct, name in permissions]))
+
         return permissions_set
