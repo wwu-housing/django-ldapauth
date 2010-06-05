f009a1de5678679c596b6c525491efeafadf8919
huddlej <huddlej@987ccdad-c2ba-db11-a1c9-001321b30054>
2010-06-05 02:08:52 +0000

    code cleanup for pylint
    git-svn-id: file:///usr/local/svn/django/trunk@4401 987ccdad-c2ba-db11-a1c9-001321b30054

--- models.py
+++ models.py
@@ -1,6 +1,13 @@
+"""
+Models for ldap authorization application.
+"""
 from django.db import models
 
 
 class LdapGroup(models.Model):
+    """
+    Represents an Active Directory token group security identifier and its
+    corresponding name.
+    """
     sid = models.CharField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
